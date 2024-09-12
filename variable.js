f9e9fd0f0a21536d3b3e9a820bccb0a9aec936fc
Daisyriee <syriefernando@gmail.com>
2024-09-12 16:53:30 +0800

    initial commit

---
+++ variable.js
@@ -0,0 +1,17 @@
+// Create a variable named name and store your name
+let name = 'Fernando, Daisyrie F.';
+
+// Create a variable named whatDoYouWannaBecomeInYourLife and store "programmer"
+let whatDoYouWannaBecomeInYourLife = 'programmer';
+
+// Create a variable named gender and store your gender
+let gender = 'Female';
+
+// Create a variable named TwitterHandle and store your Twitter Handle
+let TwitterHandle = '@syriefernando';
+
+// Log all the variables to the console
+console.log('What is your Name? ', name);
+console.log('What do you wanna become in your life? ',whatDoYouWannaBecomeInYourLife);
+console.log('Are you Male or Female? ', gender);
+console.log('My Twitter: ', TwitterHandle);
